342403ee85fa6cc04b18d114773b7e68034e7b0e
DewelG <you@example.com>
2023-10-27 10:29:39 -0400

    New updates

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from tkinter import StringVar, TOP
 import tkinter as tk
 from tkinterdnd2 import TkinterDnD, DND_ALL
-from PyPDF2 import PdfReader
+from PyPDF2 import PdfFileReader
 import customtkinter as ctk
-import re
+from customtkinter import filedialog
 
 class Tk(ctk.CTk, TkinterDnD.DnDWrapper):
     def __init__(self, *args, **kwargs):
@@ -15,20 +14,21 @@ ctk.set_appearance_mode("dark")
 def get_path(event):
     pathLabel.configure(text=event.data)
 
-def extract_and_display_dates(pdf_path):
+def extract_and_display_dates(pdf_path, re):
     dates = []
     with open(pdf_path, 'rb') as pdf_file:
-        pdf_reader = PdfReader(pdf_file)
+        pdf_reader = PdfFileReader(pdf_file)
 
         date_pattern = r'\d{1,2}/\d{1,2}/\d{2,4}'  # A basic date pattern, modify as needed
         date_regex = re.compile(date_pattern)
 
-        for page_num in range(pdf_reader.pages):
+        for page_num in range(len(pdf_reader.pages)):
             page_text = pdf_reader.getPage(page_num).extractText()
             dates.extend(date_regex.findall(page_text))
 
     # Handle the extracted dates as needed (e.g., display them)
 
+#Size and Name on interface
 root = Tk()
 root.geometry("750x360")
 root.title("Get file path")
@@ -45,7 +45,7 @@ entryWidget.drop_target_register(DND_ALL)
 entryWidget.dnd_bind("<<Drop>>", get_path)
 
 # Button to trigger PDF processing
-button = ctk.CTkButton(root, text="Read my PDF", command=lambda: extract_and_display_dates(pathLabel.cget("text")))
+button = ctk.CTkButton(root, text="Read my PDF", command=lambda: extract_and_display_dates(pathLabel("text")))
 button.pack(side=tk.TOP, padx=5, pady=5)
 
 root.mainloop()
