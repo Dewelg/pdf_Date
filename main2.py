7ed9e96cb15dc3cce40f1f33d901e056f5ac4e31
DewelG <you@example.com>
2023-11-30 13:31:12 -0500

    debugg pdf reader

--- main2.py
+++ main2.py
@@ -1,6 +1,14 @@
 import tkinter
 import customtkinter
 from tkinter import filedialog
+import re
+
+def openFile():
+    fileInput = filedialog.askopenfile()
+   
+
+def pdf_Run():
+    readFile = re.search(r'\d{2}-\d{2}-\d{4}')
 
 
 root = tkinter.Tk()
@@ -8,11 +16,10 @@ root.geometry("780x360")
 root.title("PDF reader")
 customtkinter.set_appearance_mode("Dark")
 
-fileInput = filedialog.askopenfile()
-
-button = customtkinter.CTkButton(master=fileInput, text="Open file")
+button = customtkinter.CTkButton(master=root, text="Open file", command=openFile)
 button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
-
+run_PDF = customtkinter.CTkButton(root, text="Run PDF", command=pdf_Run)
+run_PDF.pack(padx=20, pady=20)
 
 root.mainloop()
